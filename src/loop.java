517ee81e3ce26929bf036d2e6cea7b7d92d5eb54
ashuk940052 <ec19101@global.org.in>
2022-12-16 20:58:50 +0530

    initial version

---
+++ src/loop.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class loop {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter any number :");
+        int n = sc.nextInt();
+        for(int i=1 ; i<=n ; i++ ){
+            System.out.println(i+": Prashant kumar");
+        }
+    }
+}
