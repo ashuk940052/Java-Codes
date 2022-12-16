330d2d94c3eeba74491d966bd85a73566ed89938
ashuk940052 <ec19101@global.org.in>
2022-12-16 22:21:10 +0530

    initial version

--- src/loop.java
+++ src/loop.java
@@ -4,9 +4,13 @@ public class loop {
         Scanner sc = new Scanner(System.in);
         System.out.print("enter any number :");
         int n = sc.nextInt();
-        for(int i=1 ; i<=n ; i++ ){
-            System.out.println(i+": Prashant kumar");
+//        for(int i=1 ; i<=n ; i++ ){
+//            System.out.println(i+": Prashant kumar");
 
+        int i=1 ;
+        while(i<=10){
+            System.out.println(n+"*"+i+"="+i*n);
+            i++;
         }
     }
 }
