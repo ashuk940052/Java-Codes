b1f2d412073939c3ea4aeb69902a65a4d13f8403
ashuk940052 <ec19101@global.org.in>
2022-12-16 22:56:58 +0530

    initial version

---
+++ src/Pattern_3.java
@@ -0,0 +1,12 @@
+public class Pattern_3 {
+    public static void main(String[] args){
+        for(int i = 5 ; i>0 ; i--)
+        {
+            for(int j=i-1 ; j<5 ; j++ )
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
