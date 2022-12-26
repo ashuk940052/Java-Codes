6e293e9d2f1b910096ac2593397d516a9e8ae49d
ashuk940052 <ec19101@global.org.in>
2022-12-26 23:56:40 +0530

    initial version

---
+++ src/Set_0_1.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class Set_0_1 {
+    public static void set(int[] arr){
+        int count0 =0;
+        int p=arr.length;
+        int[] arrn =new int[p];
+        for(int i=0 ; i<p ; i++) {
+            if (arr[i] == 0) {
+                count0++;
+            }
+        }for(int i=0 ; i<count0 ; i++){
+            arrn[i]=0;
+        }
+        for (int i =count0 ; i<p ; i++){
+            arrn[i]=1;
+        }
+        for (int i =0; i<p ; i++){
+            System.out.print(arrn[i]+"");
+        }
+
+
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n= sc.nextInt();
+        int[] arr = new int[n];
+        for( int i=0 ; i<n ; i++){
+            arr[i]= sc.nextInt();
+
+        }  set(arr);
+    }
+
+}
