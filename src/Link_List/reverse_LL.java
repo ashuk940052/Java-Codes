d6dec4f9cff88297d33c45ad99c1b15ef7f4986f
ashuk940052 <ec19101@global.org.in>
2023-02-09 14:53:07 +0530

    DSA

--- src/Link_List/reverse_LL.java
+++ src/Link_List/reverse_LL.java
@@ -2,6 +2,10 @@ package Link_List;
 
 import java.util.Scanner;
 
+class DoubleNode{
+    Node<Integer> head;
+    Node<Integer> tail;
+}
 public class reverse_LL {
     public static Node<Integer> takeInput(){
         Node<Integer> head = null , tail = null ;
@@ -40,10 +44,38 @@ public class reverse_LL {
         head.next = null;
         return smallans;
     }
+    public static Node<Integer> Reverse_LL3(Node<Integer> head){
+        if(head == null || head.next == null){
+            return head ;
+        }
+        Node<Integer> reversed_Tail = head.next;
+        Node<Integer> smallerans = Reverse_LL3(head.next);
+        reversed_Tail.next = head;
+        head.next =null;
+        return smallerans;
+    }
+    public static DoubleNode Reverse_LL_Better(Node<Integer> head){
+        if(head == null || head.next == null){
+            DoubleNode ans = new DoubleNode();
+            ans.head = head;
+            ans.tail = head ;
+            return ans;
+        }
+        DoubleNode smallans = Reverse_LL_Better(head.next);
+        smallans.tail.next =head;
+        head.next= null;
+
+        DoubleNode ans = new DoubleNode();
+        ans.head = smallans.head;
+        ans.tail =head;
+        return ans;
+    }
 
     public static void main(String[] args) {
        Node<Integer> head = takeInput();
-       head = Reverse_LL(head);
+       head = Reverse_LL3(head);
+       DoubleNode ans = new DoubleNode();
+       ans = Reverse_LL_Better(head);
         print_LL(head);
     }
 }
