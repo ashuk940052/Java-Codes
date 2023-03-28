7ffc07be8299b9b67b0f64b2266bf0472f51f91f
ashuk940052 <ec19101@global.org.in>
2023-03-28 12:27:25 +0530

    DSA

--- src/Priority_Queue/Priority_Queue_Use.java
+++ src/Priority_Queue/Priority_Queue_Use.java
@@ -1,4 +1,6 @@
 package Priority_Queue;
+import java.util.PriorityQueue;
+import java.util.Scanner;
 
 public class Priority_Queue_Use  {
     public static void insertVertual_Heap(int[] arr, int i){
@@ -22,8 +24,8 @@ public class Priority_Queue_Use  {
       arr[0]= arr[Heapsize-1];
       Heapsize--;
       int index = 0;
-      int leftChildIndex = 2 * index + 1;
-      int rightChildIndex = 2 * index +2;
+      int leftChildIndex = 2*index+ 1;
+      int rightChildIndex = 2*index+2;
       while (leftChildIndex<Heapsize){
           int minIndex = index;
           if (arr[leftChildIndex] < arr[minIndex]){
@@ -47,17 +49,47 @@ public class Priority_Queue_Use  {
       return temp;
 
     }
+    public static void sortKSort(int[] arr , int k ){
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        int i=0;
+        for( ; i<k ;i++ ){
+            pq.add(arr[i]);
+        }
+        for( ; i<arr.length ;i++){
+            arr[i-k] = pq.remove();
+            pq.add(arr[i]);
+        }
+        for(int j = arr.length-k ; j<arr.length;j++){
+            arr[j]= pq.remove();
+        }
+    }
     public static void main(String[] args) {
-        int[] arr = {5,1,9,2,0,6};
-        for (int i=0 ; i<arr.length ;i++){
-            insertVertual_Heap(arr,i);
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of the array : ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i=0; i<n ; i++){
+            arr[i] = sc.nextInt();
         }
-        for (int i=0 ; i<arr.length ;i++){
-            arr[arr.length - 1 - i ] = removeMINVertual_Heap(arr, arr.length-i);
+        sortKSort(arr,3);
+        for (int i=0; i<n ; i++){
+            System.out.print(arr[i] +" ");
         }
-        for (int i=0 ; i<arr.length ;i++){
-            System.out.print(arr[i]+" ");
-        }
+
+
+
+
+
+        //        int[] arr = {5,1,9,2,0,6};
+//        for (int i=0 ; i<arr.length ;i++){
+//            insertVertual_Heap(arr,i);
+//        }
+//        for (int i=0 ; i<arr.length ;i++){
+//            arr[arr.length - 1 - i ] = removeMINVertual_Heap(arr, arr.length-i);
+//        }
+//        for (int i=0 ; i<arr.length ;i++){
+//            System.out.print(arr[i]+" ");
+//        }
 
 
         //        Priority_queue obj = new Priority_queue();
