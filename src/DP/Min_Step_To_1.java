8dd53cf310b9a58a3d1e49f5aaa002009402253b
ashuk940052 <ec19101@global.org.in>
2023-04-06 02:01:36 +0530

    DSA

--- src/DP/Min_Step_To_1.java
+++ src/DP/Min_Step_To_1.java
@@ -7,8 +7,7 @@ public class Min_Step_To_1 {
         if(n == 1){
             return 0;
         }
-        int op1 = countSteps(n-1);
-        int minSteps = op1;
+        int minSteps = countSteps(n-1);
         if( n%3==0){
             int op2 = countSteps(n/3);
             if(op2 < minSteps){
@@ -23,11 +22,41 @@ public class Min_Step_To_1 {
         }
         return 1+minSteps;
     }
+    public static int countStepsM(int n){
+        int[] storage = new int[n+1];
+        return countStepsM(n , storage);
+    }
+    private static int countStepsM(int n , int[] Storage){
+        if(n==1){
+            Storage[n] = 0;
+            return Storage[n];
+        }
+        if(Storage[n] != 0){
+            return Storage[n];
+        }
+        int minSteps = countStepsM(n-1,Storage);
+        if( n%3==0){
+            int op2 = countStepsM(n/3,Storage);
+            if(op2 < minSteps){
+                minSteps = op2;
+            }
+        }
+        if( n%2==0){
+            int op3 = countStepsM(n/2,Storage);
+            if(op3 < minSteps){
+                minSteps = op3;
+            }
+        }
+        Storage[n] =1+minSteps;
+        return Storage[n];
+
+    }
     public static void main(String[] args) {
      Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number : ");
      int n = sc.nextInt();
      int ans = countSteps(n);
-        System.out.print(ans);
+        System.out.println(ans);
+        System.out.println(countStepsM(n));
     }
 }
